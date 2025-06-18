7bc9e4d9a1b2af55d6ab38835356dce08da7f808
devgsheep <dev.gsheep@gmail.com>
2025-06-18 16:08:40 +0900

    [docs] 타입스크립트 기본자료형

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
-const a = "안녕";
+const strVar = "string";
+const numVar = 0;
+const boolVar = true;
+const symVar = Symbol("age");
+const nullVar = null;
+const undeVar = undefined;
+const bigIntVar = BigInt(9999999);
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-const a = "안녕";
+let neverVar: never = 100;
+neverVar1: never = 100;
+neverVar2: never = true;
+neverVar3: never = undefined;
+neverVar4: never = null;
+
+// 전달 시 unknown은 타입오류 발생
+let testNum: number = neverVar;
