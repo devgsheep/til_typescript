b5bf1887f8c2e4ddb8c5290e17ff704c7df25d25
devgsheep <dev.gsheep@gmail.com>
2025-06-20 12:54:19 +0900

    [docs] 함수 기초

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,44 @@
-const strVar = "string";
-const numVar = 0;
-const boolVar = true;
-const symVar = Symbol("age");
-const nullVar = null;
-const undeVar = undefined;
-const bigIntVar = BigInt(9999999);
+let numA = 5;
+let numB = 8;
+const resultPlus = numA + numB;
+const resultMinus = numA - numB;
+const resultDevide = numA / numB;
+const resultMulti = numA * numB;
+/**
+ * 사칙연잔 함수
+ * - 숫자 2개를 입력하시면 결과가 나옵니다.
+ * - 기호는 4가지(+, -, *, /)를 사용하실수 있습니다.
+ *
+ * 사용 예시
+ * ```javascript
+ * const result = calc(5, 4, "+");
+ * ```
+ * @param {number} a
+ * @param {number} b
+ * @param {string} sign
+ * @returns {number}
+ */
+function calc(a, b, sign) {
+    let result;
+    switch (result) {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            result = a / b;
+            break;
+        default:
+            console.log("결과값오류");
+            break;
+    }
+    return result;
+}
+const result = calc(7, 10, "*");
 export {};
+//
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,57 @@
-type Good = { title: string; price: number; sale: boolean };
+let numA: number = 5;
+let numB: number = 8;
 
-const goodArr: Good[] = [
-  { title: "사과", price: 1000, sale: true },
-  { title: "딸기", price: 5000, sale: false },
-  { title: "메로나", price: 500, sale: true },
-];
-goodArr[0].title;
-const goodArrGeneric: Array<Good> = [
-  { title: "사과", price: 1000, sale: true },
-  { title: "딸기", price: 5000, sale: false },
-  { title: "메로나", price: 500, sale: true },
-];
-goodArrGeneric[0].title;
+const resultPlus: number = numA + numB;
+const resultMinus: number = numA - numB;
+const resultDevide: number = numA / numB;
+const resultMulti: number = numA * numB;
+
+/**
+ * 사칙연잔 함수
+ * - 숫자 2개를 입력하시면 결과가 나옵니다.
+ * - 기호는 4가지(+, -, *, /)를 사용하실수 있습니다.
+ *
+ * 사용 예시
+ * ```javascript
+ * const result = calc(5, 4, "+");
+ * ```
+ * @param {number} a
+ * @param {number} b
+ * @param {string} sign
+ * @returns {number}
+ */
+function calc(a: number, b: number, sign: string): number {
+  let result;
+
+  switch (sign) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "/":
+      if (a === 0) {
+        console.log("a는 절대로 0이면 안됩니다.");
+        result = 0;
+      } else if (b === 0) {
+        console.log("b는 절대로 0이면 안됩니다.");
+        result = 0;
+      } else {
+        result = a / b;
+      }
+
+      result = a !== 0 && b !== 0 ? a / b : 0;
+      // a !== 0
+      //   ? b !== 0
+      //     ? (result = a / b)
+      //     : console.log("0은 안됩니다.")
+      //   : console.log("0은 안됩니다.");
+      break;
+    case "*":
+      result = a * b;
+      break;
+  }
+  return result;
+}
+const result: number = calc(7, 10, "*");
